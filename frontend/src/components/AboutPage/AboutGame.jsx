df1371cb3bacef7945f9427d3f8e7216ccce1d0d
Nikolai <niki_kaloianov@abv.bg>
2023-12-26 17:46:43 +0200

    change aboutGame Page

--- frontend/src/components/AboutPage/AboutGame.jsx
+++ frontend/src/components/AboutPage/AboutGame.jsx
@@ -1,125 +1,177 @@
-import React, { useContext } from 'react'
+import React, { useCallback, useContext, useEffect, useRef, useState } from 'react'
 import './AboutGame.css'
-import { Link, useNavigate } from 'react-router-dom'
+import { useParams, Link, useNavigate } from 'react-router-dom'
 import { ActiveStatus } from '../../ActiveStatus'
+import { FaStar, FaHeadset } from "react-icons/fa";
+import { RxCountdownTimer } from "react-icons/rx";
+import { FaBasketShopping } from "react-icons/fa6";
+import { IoShieldCheckmarkOutline } from "react-icons/io5";
+import axios from 'axios'
 
-import { FaCartShopping, FaStar } from "react-icons/fa6";
-import { MdAdd, MdOutlineRemove } from "react-icons/md";
+import multiPlayer from '../../assets/multiPlayer.png'
+import Simulation from '../../assets/Simulation.png'
+import Adventure from '../../assets/Adventure.png'
+import Strategy from '../../assets/strategy.png'
+import Survival from '../../assets/Survival.png'
+import Fighting from '../../assets/Fighting.png'
+import Hunting from '../../assets/Hunting.png'
+import Action from '../../assets/Action.png'
+import Puzzle from '../../assets/Puzzle.png'
+import Sports from '../../assets/Sports.png'
+import BattleRoyale from '../../assets/Battle Royale.png'
+import RolePlaying from '../../assets/Role-Playing.png'
+import FirstPerson from '../../assets/First-Person.png'
+import OpenWorld from '../../assets/Open World.png'
 
-import AboutGameJS from '../../hooks/AboutGame'
+import threads from '../../assets/threads.png'
+import twitter from '../../assets/twitter.png'
+import instagram from '../../assets/instagram.png'
+import facebook from '../../assets/facebook.png'
+import telegram from '../../assets/telegram.png'
+
+import discount from '../../assets/discount.png'
+
+import AboutGameJS from '../../hooks/AboutGame';
+
 const AboutGame = () => {
     const navigate = useNavigate()
-    const { gamesData, cartData, isActive } = useContext(ActiveStatus)
-
     const {
-        conWidth,
-        gameWidth,
-        moreGamesWidth,
-        gameData,
+        gameRef,
+        gameImgSize,
+        correctIconCategory,
         handleAddGame,
-        handleRemoveGame,
-        name,
-        moreGames,
         loadingData,
+        handleRemoveGame,
+        recommendGameRef,
+        gameData,
+        recommendedGameImgSize,
+        socials,
+        icons,
     } = AboutGameJS()
+    const {
+        isActive,
+        cartData,
+        gamesData,
+    } = useContext(ActiveStatus)
 
   return (
+    <>
+    {/* <div className='background'></div> */}
     <div className='AboutGame'>
-      <div className='con' ref={conWidth}>
-        <div className='about'>
-            <div className='game'>
-                <div className='img' style={{width: gameWidth, minWidth: gameWidth, height: gameWidth * 1.5}}>
-                    <img src={gameData.image} alt="" />
-                </div>
-                <div className='info'>
-                    <div className='name'>
+        <div className='con' ref={gameRef}>
+            <div className='games'>
+                <div className='game'>
+                    <div className='img' style={{minWidth: gameImgSize, height: gameImgSize * 1.5}}>
+                        <img src={gameData.image} alt="" />
+                    </div>
+                    <div className='text'>
                         <h2>{gameData.name}</h2>
-                        <span>{gameData.price}$</span>
-                    </div>
-                    <strong>{gameData.category}</strong>
-                    <h3>Description</h3>
-                    <p>{gameData.description}</p>
-                    <div className='stars'>
-                        {Array.from({ length: gameData.star }, (_, index) => (
-                            <FaStar key={index} className='star'/>
-                        ))}
+                        <div className='starsCon'>
+                            {Array.from({length: gameData.star}, (_, index) => (
+                                <FaStar id='star' key={index}/>
+                            ))}
+                        </div>
+                            <p>{gameData.star} / 5 rating</p>
+                        <div className='category'>
+                            <h2>{gameData.category}</h2>
+                            <img src={correctIconCategory} alt="" />
+                        </div>
+                        <div className='discount'>
+                            <strong>{gameData.price}$</strong>
+                            <img src={discount} alt="" />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='stiky mobile'>
-                {isActive &&
-                    <div className='box'>
-                        <div className='header'>
-                            <h2>Manage Game:</h2>
-                        </div>
-                        <div className='btn'>
-                            {cartData.filter(game => game.name === name).length === 0 ? (
-                                <button className='manage' onClick={handleAddGame}><MdAdd className='symbol'/>{loadingData ? 'Adding' : 'Add'}</button>
-                            ) : (
-                                <button className='manage' onClick={handleRemoveGame}><MdOutlineRemove className='symbol'/>{loadingData ? 'Removeing' : 'Remove'}</button>
-                            )}
-                            {cartData.length > 0 &&
-                                <button className='cart' onClick={() => navigate('/usercart')}><FaCartShopping className='icon'/>Check Cart <div className='totalProducts'>{cartData.length}</div></button>
-                            }
-                        </div>
-                    </div>
-                }
-        </div>
-            <div className='moreGames' ref={moreGames}>
-                {gamesData.filter(prev => prev.category === gameData.category).length >= 10 ? (
-                <>
+                <div className='mobileOrder'>
                     <div className='header'>
-                        <h2>Recommended:</h2>
-                    </div>
-                    <div className='games'>
-                        {gamesData.filter(prev => prev.category === gameData.category && prev.name !== gameData.name).slice(0, 10).map((game, index) => (
-                            <Link to={`/game/${game.name}`} className='game' style={{width: moreGamesWidth, minWidth: moreGamesWidth}} key={index}>
-                                <div className='img' style={{width: moreGamesWidth, height: moreGamesWidth * 1.5}}>
-                                    <img src={game.image} alt="" />
-                                </div>
-                            </Link>
-                        ))}
-                    </div>
-                </>
-                ) : (
-                <>
-                    <div className='header'>
-                        <h2>Recommended:</h2>
-                    </div>
-                    <div className='games'>
-                        {gamesData.filter(prev => prev.name !== gameData.name).slice(0, 10).map((game, index) => (
-                            <Link to={`/game/${game.name}`} className='game' style={{width: moreGamesWidth, minWidth: moreGamesWidth}} key={index}>
-                                <div className='img' style={{width: moreGamesWidth, height: moreGamesWidth * 1.5}}>
-                                    <img src={game.image} alt="" />
-                                </div>
-                            </Link>
-                        ))}
-                    </div>
-                </>
-                )}
-            </div>
-        </div>
-        <div className='stiky'>
-            {isActive &&
-                <div className='box'>
-                    <div className='header'>
-                        <h2>Manage Game:</h2>
+                        <h2>{gameData.price}</h2>
+                        <p>$</p>
                     </div>
                     <div className='btn'>
-                        {cartData.filter(game => game.name === name).length === 0 ? (
-                            <button className='manage' onClick={handleAddGame}><MdAdd className='symbol'/>{loadingData ? 'Adding' : 'Add'}</button>
-                        ) : (
-                            <button className='manage' onClick={handleRemoveGame}><MdOutlineRemove className='symbol'/>{loadingData ? 'Removeing' : 'Remove'}</button>
-                        )}
-                        {cartData.length > 0 &&
-                            <button className='cart' onClick={() => navigate('/usercart')}><FaCartShopping className='icon'/>Check Cart <div className='totalProducts'>{cartData.length}</div></button>
-                        }
+                    {!isActive &&
+                        <button>Login</button>
+                    }
+                    {!cartData.filter(game => game.name === gameData.name).length > 0 ? (
+                        <button onClick={handleAddGame}>{!loadingData ? 'Add Game' : 'Adding'}</button>
+                    ) : (
+                        <button onClick={handleRemoveGame}>{!loadingData ? `Remove` : 'Removing'}</button>
+                    )}
+                    {cartData.length > 0 &&
+                        <button onClick={() => navigate('/usercart')}>{cartData.length > 0 && <div className='circle'>{cartData.length}</div>}Check Cart</button>
+                    }
                     </div>
                 </div>
-            }
+                <div className='recommended' ref={recommendGameRef}>
+                    <div className='header'>
+                        <h2>Gamers also viewed</h2>
+                    </div>
+                    <div className='recGames'>
+                        {gamesData.filter(game => game.category === gameData.category && game.name !== gameData.name).slice(0, 8).map((game, index) => (
+                            <Link to={`/game/${game.name}`} className='gameR' key={index}>
+                                <div className='img' style={{ width: recommendedGameImgSize ,minWidth: recommendedGameImgSize, height: recommendedGameImgSize * 1.5}}>
+                                    <img src={game.image} alt="" />
+                                    <div className='info'>
+                                        <p>{game.name}</p>
+                                    </div>
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+                <div className='description'>
+                    <div className='header'>
+                        <h2>Product description</h2>
+                        <div className='box'>
+                            <strong>{gameData.name}</strong>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde asperiores est accusamus totam culpa saepe magni distinctio possimus, itaque numquam optio. Voluptatem quisquam dicta rem quia eius earum consectetur? Molestias ea sit consequatur aliquid laborum explicabo quibusdam doloremque libero dolorem quia? Eius quae ratione molestiae. Quod officia quisquam aspernatur temporibus, cupiditate similique eligendi ullam nostrum tenetur eius dolores cum, est voluptas blanditiis sequi perspiciatis.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='social'>
+                    <div className='header'>
+                        <h2>Social Links</h2>
+                    </div>
+                    <div className='links'>
+                    {socials.map((social, index) => (
+                        <a href={social.link} key={index}>
+                            <img src={social.img} alt="" />
+                        </a>
+                    ))}
+                    </div>
+                </div>
+            </div>
+            <div className='sticky'>
+                <div className='price'>
+                    <h2>{gameData.price}</h2>
+                    <p>$</p>
+                </div>
+                <div className='btn'>
+                    {!isActive &&
+                        <button>Login</button>
+                    }
+                    {!cartData.filter(game => game.name === gameData.name).length > 0 ? (
+                        <button onClick={handleAddGame}>{!loadingData ? 'Add Game' : 'Adding'}</button>
+                    ) : (
+                        <button onClick={handleRemoveGame}>{!loadingData ? `Remove` : 'Removing'}</button>
+                    )}
+                    {cartData.length > 0 &&
+                        <button onClick={() => navigate('/usercart')}>{cartData.length > 0 && <div className='circle'>{cartData.length}</div>}Check Cart</button>
+                    }
+                </div>
+                <div className='icons'>
+                    {icons.map((line, index) => (
+                        <div className='line' key={index}>
+                            <div className='icon'>
+                                <line.icon />
+                            </div>
+                            <p>{line.p}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-      </div>
     </div>
+    </>
   )
 }
 
--- frontend/src/hooks/AboutGame.js
+++ frontend/src/hooks/AboutGame.js
@@ -1,51 +1,169 @@
 import { useCallback, useContext, useEffect, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ActiveStatus } from '../ActiveStatus'
+import { FaHeadset } from "react-icons/fa";
+import { RxCountdownTimer } from "react-icons/rx";
+import { FaBasketShopping } from "react-icons/fa6";
+import { IoShieldCheckmarkOutline } from "react-icons/io5";
 import axios from 'axios'
 
+import multiPlayer from '../assets/multiPlayer.png'
+import Simulation from '../assets/Simulation.png'
+import Adventure from '../assets/Adventure.png'
+import Strategy from '../assets/strategy.png'
+import Survival from '../assets/Survival.png'
+import Fighting from '../assets/Fighting.png'
+import Hunting from '../assets/Hunting.png'
+import Action from '../assets/Action.png'
+import Puzzle from '../assets/Puzzle.png'
+import Sports from '../assets/Sports.png'
+import BattleRoyale from '../assets/Battle Royale.png'
+import RolePlaying from '../assets/Role-Playing.png'
+import FirstPerson from '../assets/First-Person.png'
+import OpenWorld from '../assets/Open World.png'
+
+import threads from '../assets/threads.png'
+import twitter from '../assets/twitter.png'
+import instagram from '../assets/instagram.png'
+import facebook from '../assets/facebook.png'
+import telegram from '../assets/telegram.png'
+
+
 const AboutGame = () => {
-    const conWidth = useRef()
-    const moreGames = useRef()
-    const { gameName } = useParams()
-    const name = gameName
 
-    const { gamesData, userData, API, fetchUserData } = useContext(ActiveStatus)
-    const [gameWidth, setGameWidth] = useState(0)
-    const [moreGamesWidth, setMoreGamesWidth] = useState(0)
-
-    const handleUpdateWidth = useCallback(() => {
-        let width = conWidth.current.clientWidth
-        let widthMoreGames = moreGames.current.clientWidth
-        setMoreGamesWidth(widthMoreGames / 5.2)
-        if (window.innerWidth > 880) {
-        setGameWidth(width / 5.2)
-        }
-        if (window.innerWidth <= 880) {
-        setGameWidth(width / 3.2)
-        }
-        if (widthMoreGames <= 680) {
-        setMoreGamesWidth(widthMoreGames / 3.2)
-        }
-        if (widthMoreGames <= 380) {
-        setMoreGamesWidth(widthMoreGames / 2.1)
-        }
-    }, [])
-    useEffect(() => {
-        handleUpdateWidth()
-    }, [handleUpdateWidth])
-
-    useEffect(() => {
-        window.addEventListener("resize", handleUpdateWidth)
-        return () => {
-            window.removeEventListener("resize", handleUpdateWidth)
-        }
-    }, [handleUpdateWidth])
+    
+    const gameRef = useRef()
+    const recommendGameRef = useRef()
+    const {gameName} = useParams()
+    const {
+        gamesData,
+        cartData,
+        userData, API, fetchUserData, isActive
+    } = useContext(ActiveStatus)
     const [gameData, setGameData] = useState({})
+    const [correctIconCategory, setCorrectIconCategory] = useState(multiPlayer)
     useEffect(() => {
         const name = gameName
         let game = gamesData.find(game => game.name === name)
         setGameData(game || '')
+        switch (game?.category) {
+            case 'Multiplayer':
+                setCorrectIconCategory(multiPlayer)
+                break;
+            case 'Simulation':
+                setCorrectIconCategory(Simulation)
+                break;
+            case 'Adventure':
+                setCorrectIconCategory(Adventure)
+                break;
+            case 'Strategy':
+                setCorrectIconCategory(Strategy)
+                break;
+            case 'Survival':
+                setCorrectIconCategory(Survival)
+                break;
+            case 'Fighting':
+                setCorrectIconCategory(Fighting)
+                break;
+            case 'Hunting':
+                setCorrectIconCategory(Hunting)
+                break;
+            case 'Action':
+                setCorrectIconCategory(Action)
+                break;
+            case 'Puzzle':
+                setCorrectIconCategory(Puzzle)
+                break;
+            case 'Sports':
+                setCorrectIconCategory(Sports)
+                break;
+            case 'Battle Royale':
+                setCorrectIconCategory(BattleRoyale)
+                break;
+            case 'Role-Playing':
+                setCorrectIconCategory(RolePlaying)
+                break;
+            case 'First-Person':
+                setCorrectIconCategory(FirstPerson)
+                break;
+            case 'Open World':
+                setCorrectIconCategory(OpenWorld)
+                break;
+            default:
+                break;
+        }
     }, [gamesData, gameName])
+    const [gameImgSize, setGameImgSize] = useState(0)
+    const [recommendedGameImgSize, setRecommendedGGameImgSize] = useState(0)
+    const handleResize = useCallback(() => {
+        const width1 = gameRef.current.clientWidth
+        const width2 = recommendGameRef.current.clientWidth
+        setGameImgSize(width1 / 5)
+        setRecommendedGGameImgSize(width2 / 4.2)
+        if (window.innerWidth < 480) {
+        setGameImgSize(width1 / 3)
+        }
+        if (window.innerWidth < 420) {
+        setRecommendedGGameImgSize(width2 / 3)
+        }
+        if (window.innerWidth < 320) {
+            setRecommendedGGameImgSize(width2 / 2)
+            }
+    }, [])
+    useEffect(() => {
+        handleResize()
+    }, [handleResize])
+    useEffect(() => {
+        window.addEventListener("resize", handleResize)
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, [handleResize])
+    useEffect(() => {
+        console.log(gameData);
+    }, [gameData])
+
+    const socials = [
+        {
+          img: threads,
+          link: 'https://www.threads.net',
+        },
+        {
+          img: twitter,
+          link: 'https://twitter.com',
+        },
+        {
+          img: instagram,
+          link: 'https://www.instagram.com',
+        },
+        {
+          img: facebook,
+          link: 'https://www.facebook.com',
+        },
+        {
+          img: telegram,
+          link: 'https://web.telegram.org',
+        },
+    ]
+
+    const icons = [
+        {
+            icon: RxCountdownTimer,
+            p: 'right to return'
+        },
+        {
+            icon: FaHeadset,
+            p: '24/7 service'
+        },
+        {
+            icon: FaBasketShopping,
+            p: 'our stores'
+        },
+        {
+            icon: IoShieldCheckmarkOutline,
+            p: 'price protection'
+        },
+    ]
 
     const [loadingData, setLoadingData] = useState(false)
 
@@ -76,21 +194,23 @@ const AboutGame = () => {
                 setLoadingData(false)
             }
         } catch (error) {
-            
+            console.error(error);
         }
     }
+
   return {
-    conWidth,
-    gameWidth,
-    moreGamesWidth,
-    gameData,
+    gameRef,
+    gameImgSize,
+    correctIconCategory,
     handleAddGame,
-    handleRemoveGame,
-    name,
-    moreGames,
     loadingData,
+    handleRemoveGame,
+    recommendGameRef,
+    gameData,
+    recommendedGameImgSize,
+    socials,
+    icons,
   }
-  
 }
 
 export default AboutGame
