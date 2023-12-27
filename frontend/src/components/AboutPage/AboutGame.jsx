1d7c6a2a911f65924c21c047aec7e9d3d278479e
Nikolai <niki_kaloianov@abv.bg>
2023-12-27 05:42:57 +0200

    remove useeffect

--- frontend/src/components/AboutPage/AboutGame.jsx
+++ frontend/src/components/AboutPage/AboutGame.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 import './AboutGame.css'
 import { Link, useNavigate } from 'react-router-dom'
 import { ActiveStatus } from '../../ActiveStatus'
